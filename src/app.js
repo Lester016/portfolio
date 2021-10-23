c7a7ee8d8877dffd200dc5ef92a3c821a7bb26a1
Lester016 <lesterdeguzman1999@gmail.com>
2021-10-23 20:27:05 +0800

    Add scrollToTarget function

--- src/app.js
+++ src/app.js
@@ -4,3 +4,12 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
 });
+
+const scrollToTarget = (id) => {
+  const target = document.querySelector(`#${id}`);
+  scroll.scrollTo(target);
+};
+
+document
+  .getElementById("projectBtn")
+  .addEventListener("click", () => scrollToTarget("projects"));
